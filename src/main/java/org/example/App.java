06a9de8b999f385be116e5e7e2e4225468a50fab
liming <2271024172@qq.com>
2021-03-29 17:03:40 +0800

    liming修改第一次

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,10 @@ package org.example;
  */
 public class App 
 {
+    private String name;
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
 }
